ca1ba1b1c4f960bd2c693fd89a8cff135cb1981b
priyanshu1425 <singhpriyanshu957@gmail.com>
2022-07-21 00:48:39 +0530

    Added Level 2 encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+//////////Adding the encrypted schema///////////////////
+
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+////////Adding plugin to the schema/////////
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
