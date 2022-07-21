9ed70d0ddec55f35fcec4a6f3d49e6e7abfb1cad
priyanshu1425 <singhpriyanshu957@gmail.com>
2022-07-21 23:45:49 +0530

    Added Level 3 Encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 // console.log(process.env.SECRET);
@@ -24,10 +24,6 @@ const userSchema = new mongoose.Schema({
 
 
 
-////////Adding plugin to the schema/////////
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
@@ -42,7 +38,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -56,7 +52,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err,foundUser){
         if(err){
             console.log(err);
