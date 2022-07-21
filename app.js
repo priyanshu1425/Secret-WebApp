1d5e0474691539bbd3f9355dcc57b3c728c82b85
priyanshu1425 <singhpriyanshu957@gmail.com>
2022-07-21 23:20:30 +0530

    Added env files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+// console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -20,11 +22,11 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
+
 
 ////////Adding plugin to the schema/////////
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
